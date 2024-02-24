97d3b962c030b32cf9c1914942a3c2a60a5d0d42
tomvdbroecke <tom@tvdb.me>
2024-02-25 00:09:14 +0100

    error placeholder human readable, todos

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,24 @@ use anyhow::{Context, Result};
 #[derive(Parser)]
 struct Args {
     pattern: String,
-    path: PathBuf
+    path: String
 }
+
+/*
+ @todo:
+    1 ->    Make sure error formats are the same (from clap and anyhow for example)
+            I think we can use this resource: https://www.rustadventure.dev/introducing-clap/clap-v4/reporting-errors-via-clap
+    2 ->    Add logging
+*/
 
 fn main() -> Result<()> {
     let args: Args = Args::parse();
 
-    let lines = read_file(&args.path)
-        .with_context(|| format!("could not read file {:?}", &args.path))?;
+    let filepath = &PathBuf::from(&args.path);
+    let lines = read_file(filepath)
+        .with_context(|| format!("could not read file '{}'", &args.path))?;
     
+    // @todo: calling printLn very often in a loop can be slow, use a BufWriter
     for line in lines.flatten() {
         if line.contains(&args.pattern) {
             println!("{}", line);
