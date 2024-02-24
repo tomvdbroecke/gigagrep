d8bbdf37c0b13422454f95b301207331291cc9c6
tomvdbroecke <tom@tvdb.me>
2024-02-24 23:26:39 +0100

    better error reporting

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::{fs::File, io::{self, BufRead, BufReader, Lines}, path::PathBuf};
+use std::{fs::File, io::{BufRead, BufReader, Error, Lines}, path::PathBuf};
 use clap::Parser;
+use anyhow::{Context, Result};
 
 #[derive(Parser)]
 struct Args {
@@ -7,19 +8,22 @@ struct Args {
     path: PathBuf
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args: Args = Args::parse();
+
+    let lines = read_file(&args.path)
+        .with_context(|| format!("could not read file {:?}", &args.path))?;
     
-    if let Ok(lines) = read_file(&args.path) {
-        for line in lines.flatten() {
-            if line.contains(&args.pattern) {
-                println!("{}", line);
-            }
+    for line in lines.flatten() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
         }
     }
+
+    Ok(())
 }
 
-fn read_file(filepath: &PathBuf) -> io::Result<Lines<BufReader<File>>> {
+fn read_file(filepath: &PathBuf) -> Result<Lines<BufReader<File>>, Error> {
     let file: File = File::open(filepath)?;
     let reader: BufReader<File> = BufReader::new(file);
 
