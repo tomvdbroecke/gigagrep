5b0c7a1fb18b721fbd7ed8217569b7375ff169b8
tomvdbroecke <tom@tvdb.me>
2024-02-24 22:55:56 +0100

    reading file, print line if matched with pattern

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::{fs::File, io::{self, BufRead, BufReader, Lines}, path::PathBuf};
 use clap::Parser;
 
 #[derive(Parser)]
@@ -9,6 +9,19 @@ struct Args {
 
 fn main() {
     let args: Args = Args::parse();
+    
+    if let Ok(lines) = read_file(&args.path) {
+        for line in lines.flatten() {
+            if line.contains(&args.pattern) {
+                println!("{}", line);
+            }
+        }
+    }
+}
 
-    println!("pattern: {:?}, path: {:?}", args.pattern, args.path)
+fn read_file(filepath: &PathBuf) -> io::Result<Lines<BufReader<File>>> {
+    let file: File = File::open(filepath)?;
+    let reader: BufReader<File> = BufReader::new(file);
+
+    Ok(reader.lines())
 }
