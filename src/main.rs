fdc244b9395a038e76059534885b3bc05646af39
tomvdbroecke <tom@tvdb.me>
2024-02-25 13:51:04 +0100

    added logging

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
 use std::{fs::File, io::{self, BufRead, BufReader, Lines, Write}, path::PathBuf};
 use clap::{error::ErrorKind, CommandFactory, Parser};
 use anyhow::{Context, Result};
+use clap_verbosity_flag::Verbosity;
+use log::{debug, info};
 
 #[derive(Parser)]
 struct Args {
     pattern: String,
-    path: String
+    path: String,
+    #[command(flatten)]
+    verbose: Verbosity,
 }
-
-/*
- @todo:
-    1 ->    Add logging
-*/
 
 fn main() -> Result<()> {
     let args: Args = Args::parse();
+
+    if let Some(log_level) = args.verbose.log_level() {
+        simple_logger::init_with_level(log_level)?;
+        info!("starting with logging verbosity {}", &log_level);
+    }
 
     if let Err(error) = process_command(args) {
         let mut cmd = Args::command();
@@ -29,6 +33,7 @@ fn main() -> Result<()> {
 }
 
 fn process_command(args: Args) -> Result<(), anyhow::Error> {
+    debug!("processing command");
     let filepath: &PathBuf = &PathBuf::from(&args.path);
 
     let lines = read_file(filepath)
@@ -39,6 +44,7 @@ fn process_command(args: Args) -> Result<(), anyhow::Error> {
     
     for line in lines.flatten() {
         if line.contains(&args.pattern) {
+            debug!("line containing '{}' found", &args.pattern);
             writeln!(handle, "{}", line)?;
         }
     }
@@ -47,6 +53,7 @@ fn process_command(args: Args) -> Result<(), anyhow::Error> {
 }
 
 fn read_file(filepath: &PathBuf) -> Result<Lines<BufReader<File>>, std::io::Error> {
+    debug!("reading file");
     let file: File = File::open(filepath)?;
     let reader: BufReader<File> = BufReader::new(file);
 
