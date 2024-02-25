6bbf2e4dc91ee31880d8e57eebd307e47d634835
tomvdbroecke <tom@tvdb.me>
2024-02-25 12:25:41 +0100

    standardized logging format

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::{fs::File, io::{BufRead, BufReader, Error, Lines}, path::PathBuf};
-use clap::Parser;
+use std::{fs::File, io::{BufRead, BufReader, Lines}, path::PathBuf};
+use clap::{error::ErrorKind, CommandFactory, Parser};
 use anyhow::{Context, Result};
 
 #[derive(Parser)]
@@ -10,15 +10,27 @@ struct Args {
 
 /*
  @todo:
-    1 ->    Make sure error formats are the same (from clap and anyhow for example)
-            I think we can use this resource: https://www.rustadventure.dev/introducing-clap/clap-v4/reporting-errors-via-clap
-    2 ->    Add logging
+    1 ->    Add logging
 */
 
 fn main() -> Result<()> {
     let args: Args = Args::parse();
 
+    if let Err(error) = process_command(args) {
+        let mut cmd = Args::command();
+        if let Some(source) = error.source() {
+            cmd.error(ErrorKind::Io, format!("{}\n\ncause: {}", &error, source)).exit()
+        } else {
+            cmd.error(ErrorKind::Io, format!("{}", &error)).exit()
+        }
+    }
+
+    Ok(())
+}
+
+fn process_command(args: Args) -> Result<(), anyhow::Error> {
     let filepath = &PathBuf::from(&args.path);
+
     let lines = read_file(filepath)
         .with_context(|| format!("could not read file '{}'", &args.path))?;
     
@@ -32,7 +44,7 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn read_file(filepath: &PathBuf) -> Result<Lines<BufReader<File>>, Error> {
+fn read_file(filepath: &PathBuf) -> Result<Lines<BufReader<File>>, std::io::Error> {
     let file: File = File::open(filepath)?;
     let reader: BufReader<File> = BufReader::new(file);
 
